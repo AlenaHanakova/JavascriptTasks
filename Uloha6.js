f44e56457997e11c64345508bd2ec008a3f9bd57
Alena Hanakova <al.hanakova@gmail.com>
2024-12-05 14:40:44 +0100

    Uloha6

---
+++ Uloha6.js
@@ -0,0 +1,12 @@
+function dayDif(date1, date2) {
+    return Math.abs(Math.round((date1.getTime() - date2.getTime()) / (24 * 60 * 60 * 1000)));
+}
+
+function daysSince(date1) {
+    return Math.abs(Math.round((date1.getTime() - new Date().getTime()) / (24 * 60 * 60 * 1000)));
+}
+
+let date1 = new Date("2024-12-3");
+let date2 = new Date("2024-11-20");
+console.log(dayDif(date1, date2));
+console.log(daysSince(date1));

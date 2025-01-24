f3cbf425eb89c37fff239b6805969d1f78579c35
AlenaHanakova <40390654+AlenaHanakova@users.noreply.github.com>
2025-01-24 11:56:36 +0100

    add negative rotation option

--- javascript/Uloha2.js
+++ javascript/Uloha2.js
@@ -1,11 +1,14 @@
 function rotateArray(array, n) {
     let result = [];
+
+    if (n < 0) { n = (n % array.length) + array.length; }
+
     for (let i = 0; i < array.length; i++) {
         result[i] = array[(i + n) % array.length];
     }
     return result;
+
 }
-
-
 let testArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-console.log(rotateArray(testArray, 5));
+let nx = -5;
+console.log(rotateArray(testArray, nx));

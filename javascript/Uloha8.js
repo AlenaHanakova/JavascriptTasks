7b6f2dc396421886029a5881bdb235111e13d777
AlenaHanakova <40390654+AlenaHanakova@users.noreply.github.com>
2025-01-24 12:01:01 +0100

    Update Uloha8.js

--- javascript/Uloha8.js
+++ javascript/Uloha8.js
@@ -1,8 +1,10 @@
 function getPermutations(array) {
-    if (array.length <= 1) return array;
+    if (array.length <= 1) { return array; };
     let result = [];
     for (let i = 0; i < array.length; i++) {
+        // pole prvku ktere neobsahuji prvek na pozici i - aby se neopakoval
         let otherElements = array.slice(0, i).concat(array.slice(i + 1, array.length));
+        //vlozim vsechny mozne permutace zacinajici ar[i] a koncici vsemi permutacemi zbylych prvku
         getPermutations(otherElements).forEach(element => {
             result.push([array[i]].concat(element));
         });

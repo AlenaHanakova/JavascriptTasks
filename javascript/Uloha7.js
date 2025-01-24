a0ab773ccc3121c32824a74606e5148f83d5bb77
AlenaHanakova <40390654+AlenaHanakova@users.noreply.github.com>
2025-01-24 12:00:37 +0100

    Update Uloha7.js

--- javascript/Uloha7.js
+++ javascript/Uloha7.js
@@ -1,10 +1,10 @@
 function polSum(pol1, pol2) {
-    //prohodim polynomy, abych mela pol vyssiho stupne jako prvni - nemusim resit ze pol[i] neni inicializovano
-    if (pol1.length > pol2.length) [pol1, pol2] = [pol2, pol1];
+    // ujistim se,ze polynom s  vysim stupnem je jako prvni a tim se vyhnu nutnosti inicializovat pole
+    if (pol1.length < pol2.length) { [pol1, pol2] = [pol2, pol1]; }
 
     let result = [...pol2];
     for (let i = 0; i < pol1.length; i++) {
-        result[i] += pol1[i] + pol2[i];
+        result[i] += pol2[i];
     }
     return result;
 }
@@ -22,6 +22,7 @@ function polMul(pol1, pol2) {
     for (let i = 0; i < (pol1.length + pol2.length - 1); i++) {
         result[i] = 0;
     }
+    //nasobi se kazdy s kazdym
     for (let i1 = 0; i1 < pol1.length; i1++) {
         for (let i2 = 0; i2 < pol2.length; i2++) {
             result[i1 + i2] = pol1[i1] * pol2[i2];
